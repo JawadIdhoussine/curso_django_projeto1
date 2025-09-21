71b2e9a92bc31cf09cfdce7d516fb089e914ca09
Jawad Idhoussine <id.houssine@gmail.com>
2025-09-21 09:51:27 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'global/home.html')
+    return render(request, 'recipes/home.html')
 
 
 def sobre(request):
